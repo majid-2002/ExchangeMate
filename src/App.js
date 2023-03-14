ee8b8c7bf59932eb5f5b6805ba4a2a90840fa43e
Abdul Majid <76196721+majid-2002@users.noreply.github.com>
2023-03-15 01:10:42 +0530

    update : field dropdown and flags

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import React, { useState } from "react";
-import { CountryDropdown } from "react-country-region-selector";
+import React, {useState} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Dropdown from "react-bootstrap/Dropdown";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import countries from "country-data";
 
 function App() {
   const [country, setCountry] = useState("");
+  const [countryTwo, setCountryTwo] = useState("")
 
-  const selectCountry = (val) => {
-    setCountry(val);
-  };
+  const countryList = countries.countries.all.map((country, index) => (
+    <Dropdown.Item key={index}  onClick={()=>{
+      setCountry(country.name);
+    }}>
+      {country.emoji} {country.name}
+    </Dropdown.Item>
+  ));
+
+  const countryList2 = countries.countries.all.map((country, index) => (
+    <Dropdown.Item key={index} onClick={() => {
+      setCountryTwo(country.name);
+    }}>
+      {country.emoji} {country.name}
+    </Dropdown.Item>
+  ));
+
 
   return (
     <div className="input-area">
-    <div className="content-area">
-      <CountryDropdown
-        value={country}
-        onChange={selectCountry}
-        classes="btn-primary btn mt-4"
-      />
-    </div>
+      <div className="content-area">
+        <InputGroup className="my-3">
+          <Dropdown>
+            <Dropdown.Toggle
+              id="dropdown-button-dark-example1"
+              variant="dark"
+              className="w-50"
+            >
+              {country === "" ? "Country" : country}
+            </Dropdown.Toggle>
+            <Dropdown.Menu variant="dark" className=" w-50 dropdown-menu">
+              {countryList}
+            </Dropdown.Menu>
+          </Dropdown>
+          <Form.Control
+            aria-label="Example text with button addon"
+            aria-describedby="basic-addon1"
+          />
+        </InputGroup>
+        <InputGroup className="my-3">
+          <Dropdown>
+            <Dropdown.Toggle
+              id="dropdown-button-dark-example1"
+              variant="dark"
+              className="w-50"
+            >
+            {countryTwo === "" ? "Country" : countryTwo}
+            </Dropdown.Toggle>
+            <Dropdown.Menu variant="dark" className=" w-50 dropdown-menu">
+              {countryList2}
+            </Dropdown.Menu>
+          </Dropdown>
+          <Form.Control
+            aria-label="Example text with button addon"
+            aria-describedby="basic-addon1"
+          />
+        </InputGroup>
+      </div>
     </div>
   );
 }
